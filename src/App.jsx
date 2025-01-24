f8ffbba58b4d4a672a67517570886635e7dd0d5f
AhmedSalman <medosalman375@gmail.com>
2025-01-24 08:45:16 +0200

    feat: complete navigation and add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
     >
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
