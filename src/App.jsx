278420198757d90c34d22e069b10c41271878082
AhmedSalman <medosalman375@gmail.com>
2025-01-24 19:47:42 +0200

    feat: add dynamic routes with URL params

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import PageNotFound from './pages/PageNotFound'
 import Login from './pages/Login'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
+import City from './components/City'
 
 polyfillCountryFlagEmojis()
 
@@ -55,6 +56,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
