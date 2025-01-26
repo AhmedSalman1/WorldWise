39c1bc44fd7c2328feed02952d3d742954bac3fd
AhmedSalman <medosalman375@gmail.com>
2025-01-26 08:09:30 +0200

    feat(city): add functionality to create and delete a city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -18,7 +18,7 @@ function CitiesProvider({ children }) {
 
         setCities(data)
       } catch {
-        alert('There was an error loading cities')
+        alert('There was an error loading data...')
       } finally {
         setIsLoading(false)
       }
@@ -35,14 +35,60 @@ function CitiesProvider({ children }) {
 
       setCurrentCity(data)
     } catch {
-      alert('There was an error loading cities')
+      alert('There was an error loading data...')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true)
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: 'POST',
+        body: JSON.stringify(newCity),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await res.json()
+
+      setCities((cities) => [...cities, data])
+
+      // console.log(data)
+    } catch {
+      alert('There was an error creating city.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true)
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      })
+
+      setCities((cities) => cities.filter((city) => city.id !== id))
+    } catch {
+      alert('There was an error deleting city.')
     } finally {
       setIsLoading(false)
     }
   }
 
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
+    >
       {children}
     </CitiesContext.Provider>
   )
